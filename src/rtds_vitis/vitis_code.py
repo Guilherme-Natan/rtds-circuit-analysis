7b70e9686ea834fa0690338dad761f7215451537
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-11-24 14:37:04 -0300

    Add the main function for rtds-vitis

--- src/rtds_vitis/vitis_code.py
+++ src/rtds_vitis/vitis_code.py
@@ -1,4 +1,5 @@
 import sys
+from os.path import basename, splitext
 from typing import TYPE_CHECKING
 
 import sympy as sp
@@ -109,6 +110,90 @@ def get_cpp_parameters(parameters: list[str]) -> str:
     return output
 
 
+def get_inputs_and_states(equations: dict[str, sp.Eq]) -> tuple[list[str], list[str]]:
+    """Gets the state variables, and the inputs, that will be used in the fpga simulation.
+
+    Args:
+        equations (dict[str, sp.Eq]): The state equations for the circuit.
+
+    Returns:
+        tuple[list[str], list[str]]: The state variable, and the inputs.
+    """
+    # Get states
+    states = [f"V{component}" if component[0] == "C" else f"I{component}" for component in equations]
+    states = sorted(states)
+
+    # Get inputs
+    inputs = set()
+    for equation in equations.values():
+        symbols = {str(symbol) for symbol in equation.free_symbols}
+        # Remove parameters and states
+        symbols = {symbol for symbol in symbols if (symbol[0] in ("V", "I")) and (symbol[:2] not in ("VC", "IL"))}
+        inputs.update(symbols)
+
+    # Remove _{n} and _{n-1} from the input names. This will also deduplicate those variables in case the method chosen
+    # is the trapezoidal one (there would be one variable for _{n-1}, and another for _{n}).
+    inputs_no_subscripts = set()
+    for i in inputs:
+        if i[-4:] == "_{n}":
+            i = i[:-4]
+        else:
+            i = i[:-6]
+            inputs_no_subscripts.add(i)
+
+    inputs = sorted(list(inputs_no_subscripts))
+    return inputs, states
+
+
+def define_function(filepath: str, inputs: list[str], states: list[str]) -> str:
+    """Defines the function used in by the Vitis Software for the RT Simulation.
+
+    Args:
+        filepath (str): The path for the netlist file
+        inputs (list[str]): The inputs for the function
+        states (list[str]): The states for the function
+
+    Returns:
+        str: The statement that defines the function
+    """
+
+    # Takes only the basename (without extension) for the name for the function
+    name = splitext(basename(filepath))[0]
+
+    # Write the inputs as parameters for the function
+    parsed_inputs = ""
+    for i in inputs:
+        parsed_inputs += f" data_t {i},"
+
+    # Write the inputs as parameters for the function
+    parsed_states = ""
+    for s in states:
+        parsed_states += f" data_t *{s},"
+
+    return f"\nvoid {name}(uint1_t sinc,{parsed_inputs}{parsed_states})" + "{"
+
+
+def main_function(equations: dict[str, sp.Eq], args: "Namespace") -> str:
+    """Writes the main function to be used by Vitis HLS
+
+    Args:
+        equations (dict[str, sp.Eq]): The equations to add to the function
+        args (Namespace): The arguments given for rtds-vitis
+
+    Returns:
+        str: The main function
+    """
+
+    inputs, states = get_inputs_and_states(equations)
+
+    # Starts the main function
+    code = define_function(args.filepath, inputs, states)
+
+    # Closes the main function
+    code += "\n}"
+    return code
+
+
 def print_vitis_code(circuit: "Circuit", args: "Namespace"):
     """Prints the cpp vitis code, for implementing the circuit in an FPGA.
 
@@ -128,11 +213,14 @@ def print_vitis_code(circuit: "Circuit", args: "Namespace"):
     if parameters:
         code += get_cpp_parameters(parameters)
 
-    print(code)
+    # Write the main function
+    code += main_function(equations, args)
 
+    # Prints resulting code
+    print(code)
     if parameters:
         print(
-            "\033[33mWARNING: Components with literal values found. You'll need to replace all the "
+            "\n\033[33mWARNING: Components with literal values found. You'll need to replace all the "
             "'CHANGEME's in the cpp code with their respective numeric values\033[0m",
             file=sys.stderr,
         )
